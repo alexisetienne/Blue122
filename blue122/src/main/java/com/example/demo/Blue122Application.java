649b46a355f89cf38a807f8fe5e48960094bf054
hello <hello@redmush>
2019-04-24 16:07:21 +0200

    bleu

---
+++ blue122/src/main/java/com/example/demo/Blue122Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blue122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blue122Application.class, args);
+	}
+
+}
---
+++ blue122/src/main/java/com/example/demo/PagesControler.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PagesControler {
+	
+	@getmapping
+	public String home()
+	
+	
+}
+
+}
